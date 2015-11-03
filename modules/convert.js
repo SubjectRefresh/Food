2731331b4e9c562a957adf3ae6fe991b3b90284e
Finnian Anderson <finnian@fxapi.co.uk>
2015-11-03 14:29:23 +0000

    Move @popey456963's code to correct location

--- modules/convert.js
+++ modules/convert.js
@@ -12,9 +12,37 @@ module.exports = {
 		});
 	},
 
-	convertKCalToRequriedSport: function(calories, callback){
-		// returns how many km need to be traveled for walk, cycle and swim
+	getCalorie: function(ndb, callback){
+		request('http://api.nal.usda.gov/ndb/reports/?ndbno=' + ndb + '&type=f&format=json&api_key=UqyGauU4aSQSifon8gyVK5riyMn5ubZinIHBOk5B', function (error, response, body) {
+	        var data = JSON.parse(body).report.food.nutrients;
+	        var energy = [];
+	        var parsedEnergy = [];
+	        for ( i=0; i<data.length; i++ ) {
+	                if ( data[i].name == "Energy") {
+	                        if ( data[i].unit == "kcal") {
+	                                energy = data[i];
+	                        }
+	                }
+	        }
+	        parsedEnergy['calories'] = [];
+	        parsedEnergy['calories']['value'] =  energy.value;
+	        parsedEnergy['calories']['measure'] = "100";
+	        callback(parsedEnergy);
+	    });
+	},
 
-		if (callback) callback({walk: "2.4km", cycle: "1km", swim: "0.5k", calories: calories});
+	energyBurnt: function(age, weight, exercise, time, gender, callback){
+		var heartrate = 0;
+        if ( exercise == "walking" ) {
+        	heartrate = 130;
+        } else if ( exercise == "running") {
+        	heartrate = 170;
+        }
+        if ( gender == "man" ) {
+        	var CaloriesBurnt = (age * 0.2017 - weight * 0.09036 + heartrate * 0.6309 - 55.0969) * (time / 4.184);
+        } else {
+        	var CaloriesBurnt = (age * 0.074 - weight * 0.05741 + heartrate * 0.4472 - 20.4022) * (time / 4.184);
+        }
+        callback(CaloriesBurnt);
 	}
 };
