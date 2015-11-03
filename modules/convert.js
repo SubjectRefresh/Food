d44da90e0dfe57637b4a6576b4cd5e21f15bdefa
Oli Callaghan <OliCallaghan@icloud.com>
2015-11-03 15:35:48 +0000

    dsafdsdsaf

--- modules/convert.js
+++ modules/convert.js
@@ -22,14 +22,12 @@ module.exports = {
 	        for ( i=0; i<data.length; i++ ) {
 	                if ( data[i].name == "Energy") {
 	                        if ( data[i].unit == "kcal") {
-                                energy.push(data[i]);
+                                energy = data[i];
 	                        }
 	                }
 	        }
-	        parsedEnergy['calories'] = [];
-	        parsedEnergy['calories']['value'] =  energy.value;
-	        parsedEnergy['calories']['measure'] = "100";
-	        callback(parsedEnergy);
+            console.log(energy['value']);
+            callback(energy['value']);
 	    });
 	},
 
