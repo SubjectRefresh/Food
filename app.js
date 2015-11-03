35adb032331bfe41377354ae8c8c8f7245cb5dd9
Oli Callaghan <OliCallaghan@icloud.com>
2015-11-03 14:10:29 +0000

    Added Margin to the Top of the Page

--- app.js
+++ app.js
@@ -1,94 +1,99 @@
-try {
-	var colors = require("colors");
-	var fs = require("fs");
-    var app = require("express")();
-    var http = require("http").Server(app);
-    var io = require("socket.io")(http);
-    var express = require("express");
+function heartbeat() {
+    try {
+        var colors = require("colors");
+        var fs = require("fs");
+        var app = require("express")();
+        var http = require("http").Server(app);
+        var io = require("socket.io")(http);
+        var express = require("express");
 
-    var convert = require("./modules/convert.js");    
+        var convert = require("./modules/convert.js");
 
-    var branch = "master";
+        var branch = "master";
 
-    var git = require('git-rev');
+        var git = require('git-rev');
 
-    app.use(require("express").static('public'));
+        app.use(require("express").static('public'));
 
-    app.get("/", function(req, res) {
-        console.log("app.js      - " + "[Refresh - Food] Sending homepage".blue);
-        fs.readFile("pages/index.html", "utf-8", function(err, data) {
-            res.send(data);
-        });
-        convert.convertKCalToRequriedSport(1200, function(data){
-            console.log(data);
-        });
-    });
-
-    io.on("connection", function(socket) {
-        socket.on("getFoodSuggestions", function(packet) {
-            // Do Code
-            // packet = { food: "foodname" };
-
-            // output = { foods: [{ food: "foodname", calories: 657 }, { ... }, { ... }] }
-            socket.send(output);
-        });
-
-        socket.on("sendFood", function(packet) {
-            // Do Code
-            // packet = { food: "foodname", calories: 657 };
-
-            // output = { exercise: { running: 1.5, cycling: 4.5, swimming: 6.5 }, alternative: { name: "foodname", url: "http://www.food.recipe" } }
-            socket.send(output);
-        });
-    });
-
-    app.post("/update-git", function(req, res){
-        console.log("Reloading git repo");
-        updateGit();
-        res.send({message:"OK",code:200})
-    });
-
-    function run_cmd(cmd, args, cb, end) {
-        var spawn = require('child_process').spawn,
-            child = spawn(cmd, args),
-            me = this;
-        child.stdout.on('data', function (buffer) { cb(me, buffer) });
-        child.stdout.on('end', end);
-    }
-
-    function gitFetch(callback){
-        var git_fetch_output = new run_cmd(
-            'git', ['fetch','--all'],
-            function (me, buffer) { me.stdout += buffer.toString() },
-            function(){
-                callback(git_fetch_output.stdout);
-            }
-        );
-    }
-
-    function gitReset(callback){
-        var git_reset_output = new run_cmd(
-            'git', ['reset','--hard','origin/'+branch],
-            function (me, buffer) { me.stdout += buffer.toString() },
-            function(){
-                callback(git_reset_output.stdout);
-            }
-        );
-    }
-
-    function updateGit(){
-        gitFetch(function(output){
-            console.log("app.js      - " + output.green);
-            gitReset(function(output){
-                console.log("app.js      - " + output.green);
+        app.get("/", function(req, res) {
+            console.log("app.js      - " + "[Refresh - Food] Sending homepage".blue);
+            fs.readFile("pages/index.html", "utf-8", function(err, data) {
+                res.send(data);
+            });
+            convert.convertKCalToRequriedSport(1200, function(data){
+                console.log(data);
             });
         });
-    }
 
-	http.listen(3001, function() {
-        console.log("app.js      - " + "[Refresh - Food] Running at ".green + "http://localhost:3001".blue);
-    });
-} catch (e) {
-    console.log("app.js      - " + "[Refresh] FATAL ERROR".red);
-    console.log(e);
+        io.on("connection", function(socket) {
+            socket.on("getFoodSuggestions", function(packet) {
+                // Do Code
+                // packet = { food: "foodname" };
+
+                // output = { foods: [{ food: "foodname", calories: 657 }, { ... }, { ... }] }
+                socket.send(output);
+            });
+
+            socket.on("sendFood", function(packet) {
+                // Do Code
+                // packet = { food: "foodname", calories: 657 };
+
+                // output = { exercise: { running: 1.5, cycling: 4.5, swimming: 6.5 }, alternative: { name: "foodname", url: "http://www.food.recipe" } }
+                socket.send(output);
+            });
+        });
+
+        app.post("/update-git", function(req, res){
+            console.log("Reloading git repo");
+            updateGit();
+            res.send({message:"OK",code:200})
+        });
+
+        function run_cmd(cmd, args, cb, end) {
+            var spawn = require('child_process').spawn,
+                child = spawn(cmd, args),
+                me = this;
+            child.stdout.on('data', function (buffer) { cb(me, buffer) });
+            child.stdout.on('end', end);
+        }
+
+        function gitFetch(callback){
+            var git_fetch_output = new run_cmd(
+                'git', ['fetch','--all'],
+                function (me, buffer) { me.stdout += buffer.toString() },
+                function(){
+                    callback(git_fetch_output.stdout);
+                }
+            );
+        }
+
+        function gitReset(callback){
+            var git_reset_output = new run_cmd(
+                'git', ['reset','--hard','origin/'+branch],
+                function (me, buffer) { me.stdout += buffer.toString() },
+                function(){
+                    callback(git_reset_output.stdout);
+                }
+            );
+        }
+
+        function updateGit(){
+            gitFetch(function(output){
+                console.log("app.js      - " + output.green);
+                gitReset(function(output){
+                    console.log("app.js      - " + output.green);
+                });
+            });
+        }
+
+        http.listen(3001, function() {
+            console.log("app.js      - " + "[Refresh - Food] Running at ".green + "http://localhost:3001".blue);
+        });
+    } catch (e) {
+        console.log("app.js      - " + "[Refresh] FATAL ERROR".red);
+        console.log(e);
+        heartbeat();
+    }
 }
+
+heartbeat();
