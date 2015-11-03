630640e9975240732890a008c1d3bbb6c73841ef
Finnian Anderson <finnian@fxapi.co.uk>
2015-11-03 12:08:05 +0000

    Change footer/navbar colours

--- modules/convert.js
+++ modules/convert.js
@@ -1,14 +1,13 @@
-// -> some food
-
-// <- barcode
-
 module.exports = {
 	convertBarcodeToCalories: function(barcode, callback){
-		// do magic
+		// converts barcode to calories
+
 		if (callback) callback(converted);
 	},
 
 	convertKCalToRequriedSport: function(calories, callback){
+		// returns how many km need to be traveled for walk, cycle and swim
+
 		if (callback) callback({walk: "2.4km", cycle: "1km", swim: "0.5k", calories: calories});
 	}
 };
