b852e905dc9ce673948d3ce35b3c1514b693c639
Popey Gilbert <popey@debenclipper.com>
2015-11-04 18:22:13 +0000

    object instead of hardcode

--- modules/convert.js
+++ modules/convert.js
@@ -5,11 +5,11 @@ module.exports = {
 		request('http://api.nal.usda.gov/ndb/search/?format=json&q=' + currentSearch +  '&sort=n&max=25&offset=0&api_key=UqyGauU4aSQSifon8gyVK5riyMn5ubZinIHBOk5B', function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var data = JSON.parse(body).list.item;
-				var niceData = [];
+				var simpleData = [];
 				for ( i=0; i<data.length; i++ ) {
-					niceData.push([data[i].group, data[i].name, data[i].ndbno]);
+					simpleData.push([data[i].group, data[i].name, data[i].ndbno]);
 				}
-				callback(niceData);
+				callback(simpleData);
 			}
 		});
 	},
@@ -31,14 +31,16 @@ module.exports = {
 	},
 
 	energyBurnt: function(age, weight, exercise, time, gender, callback){
-		var heartrate = 0;
-        if ( exercise == "walking" ) {
-        	heartrate = 130;
-        } else if ( exercise == "running") {
-        	heartrate = 170;
-        } else if ( exercise == "swimming"){
-        	heartrate = 220 - parseInt(age);
-        }
+		var type = {
+			"walking":130,
+			"running":170,
+			"swimming":220};
+		try {
+			var heartrate = type[exercise];
+		} catch(e) {
+			var heartrate = 100;
+		}
+
         if ( gender == "m" ) {
         	var CaloriesBurnt = (age * 0.2017 - weight * 0.09036 + heartrate * 0.6309 - 55.0969) * (time / 4.184);
         } else {
