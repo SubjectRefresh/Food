7f5a8c9a60d0778918a5ba1ea84ec6dcbc657070
Finnian Anderson <finnian@fxapi.co.uk>
2015-11-04 19:47:36 +0000

    Include Twitter 'intent' button for future use

--- public/js/main.js
+++ public/js/main.js
@@ -25,13 +25,14 @@ $(document).ready(function() {
     });
 
     socket.on("receiveCalories", function(packet) {
+//        $("#share-results").attr("href", "https://twitter.com/intent/tweet?text=I%20just%20used%20%40SubjectRefresh%27s%20%23healthy%20app!%20Try%20it%20out%20at%20http%3A%2F%2Ffood.subjectrefresh.info!");
         $("main > .container > .results").append('<div class="col s12 m4"><div class="card blue-grey darken-1 white-text"><div class="card-content"><span class="card-title">Run</span></div><div class="card-action"><span class="time-number">' + Math.floor(packet.running * 60) + '</span> minutes</i></div></div></div>');
         $("main > .container > .results").append('<div class="col s12 m4"><div class="card blue-grey darken-1 white-text"><div class="card-content"><span class="card-title">Walk</span></div><div class="card-action"><span class="time-number">' + Math.floor(packet.walking * 60) + '</span> minutes</i></div></div></div>');
         $("main > .container > .results").append('<div class="col s12 m4"><div class="card blue-grey darken-1 white-text"><div class="card-content"><span class="card-title">Swim</span></div><div class="card-action"><span class="time-number">' + Math.floor(packet.swimming * 60) + '</span> minutes</i></div></div></div>');
         $(".results").slideDown("slow");
         $("#go1").fadeOut("slow", function(){
             $(this).html("Try another").off("click").on("click", function(){
-                window.location.reload(false); 
+                window.location.reload(false);
             }).fadeIn("slow");
         });
     });
